f927c20007da741e749b6ea420a459fe6b06a797
jennyquinto <vane.quinto19@gmail.com>
2023-06-01 17:51:00 -0500

    Building mobile menu

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuButtton = document.querySelector('.mobile-nav-toggle');
+const menuList = document.querySelector('.primary-navigation');
+
+menuButtton.addEventListener('click', () => {
+    menuList.hasAttribute('data-visible')
+    ? menuList.setAttribute('aria-expanded', false)
+    : menuList.setAttribute('aria-expanded', true)
+    menuList.toggleAttribute('data-visible');
+})
