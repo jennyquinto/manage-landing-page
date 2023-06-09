ba11d58dbdaed64eac848c5148c8a1a8389e94b1
jennyquinto <vane.quinto19@gmail.com>
2023-06-08 19:23:01 -0500

    Adjustments

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ menuButtton.addEventListener('click', () => {
     menuList.hasAttribute('data-visible')
     ? menuList.setAttribute('aria-expanded', false)
     : menuList.setAttribute('aria-expanded', true)
-    menuList.toggleAttribute('data-visible');
-    mainContent.toggleAttribute('data-overlay');
+    menuList.toggleAttribute('data-visible')
+    mainContent.toggleAttribute('data-overlay')
     bodyContent.toggleAttribute('data-overlay');
 });
