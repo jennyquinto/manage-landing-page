01d484bdfb9085c176cd21a46d73454f64ce65a1
jennyquinto <vane.quinto19@gmail.com>
2023-07-05 21:14:48 -0500

    Cleaning slider script

--- main.js
+++ main.js
@@ -14,19 +14,15 @@ menuButtton.addEventListener('click', () => {
 
 const slider = new A11YSlider(document.querySelector(".slider"), {
   slidesToShow: 1,
-  arrows: false, // arrows enabled 767px and down
+  arrows: false,
   dots: true,
   responsive: {
     480: {
-      // disable: false,
       slidesToShow: 1,
-      // arrows: true,
-      // dots: false
     },
     600: {
       slidesToShow: 2,
       dots: false
-
     },
     900: {
       slidesToShow: 3,
